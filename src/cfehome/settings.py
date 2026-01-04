64737717b6ccb1d70ec07133b6bedde81a654ffa
RavinduPe <raviduy60@gmail.com>
2026-01-04 20:05:41 +0530

    Updated build args

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,8 +25,6 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", cast=bool)
 
-print("DEBUG = ", DEBUG , type(DEBUG))
-
 ALLOWED_HOSTS = [
     ".railway.app" #https://saas.prod.railway.app
 ]
